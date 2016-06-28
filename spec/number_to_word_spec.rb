7e2415f5b3fe43e3338248af10b678cb8ad70495
Caleb Stokka and Tony Fabery <caleb+tony@>
2016-06-28 09:50:28 -0700

    returns a word for a number less than one thousand

--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -9,9 +9,12 @@ describe('Fixnum#num_to_word') do
     expect(18.num_to_word()).to(eq('eighteen'))
   end
   it("returns a word for a number less than one hundred") do
-    expect(54.num_to_word()).to(eq('fifty four'))
+    expect(054.num_to_word()).to(eq('fifty four'))
   end
   it("returns a word for a number less than one thousand") do
     expect(678.num_to_word()).to(eq('six hundred and seventy eight'))
+  end
+  it("returns a word for a number less than one million") do
+    expect(83746.num_to_word()).to(eq('eighty three thousand, seven hundred and forty six'))
   end
 end
