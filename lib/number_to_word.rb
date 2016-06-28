b4bc8b703e7fb5609f67e792844355b9616801d7
Caleb Stokka and Tony Fabery <caleb+tony@>
2016-06-28 09:43:34 -0700

    returns a word for a number less than one hundred

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -3,9 +3,15 @@ require('pry')
 class Fixnum
   define_method(:num_to_word) do
     num_hash_ones = {0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
-    num_hash_tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 20 => 'twenty', 30 => 'thirty', 40 => 'forty', 50 => 'fifty', 60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'}
-    num_hash_ones[self]
-    if self >= 10
-    # num_hash.each
+    num_hash_tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
+    word = ''
+    if self < 10
+      word = num_hash_ones[self]
+    elsif self <= 20
+      word = num_hash_tens[self]
+    elsif self < 100
+      word = num_hash_tens[self./(10)] + ' ' + num_hash_ones[self.%(10)]
+    end
+    word
   end
 end
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -8,4 +8,7 @@ describe('Fixnum#num_to_word') do
   it("returns a word for a number up to twenty") do
     expect(18.num_to_word()).to(eq('eighteen'))
   end
+  it("returns a word for a number less than one hundred") do
+    expect(54.num_to_word()).to(eq('fifty four'))
+  end
 end
