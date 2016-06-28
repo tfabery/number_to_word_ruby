775640bde45962971ba94c5c233c41f2bef65ddb
Caleb Stokka and Tony Fabery <caleb+tony@>
2016-06-28 11:30:20 -0700

    returns a word for a number less than one million

--- lib/number_to_word.rb
+++ lib/number_to_word.rb
@@ -3,28 +3,36 @@ require('pry')
 class Fixnum
   define_method(:num_to_word) do
     num_hash_ones = {0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
-    num_hash_tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
-    word = ''
+    num_hash_tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 1 => 'ten', 2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
+    word = []
     new_num = self
     # binding.pry()
-    while new_num > 1000 do
-      word = num_hash_ones[new_num./(1000)] + " thousand "
+    while new_num >= 100000 do
+      word.push(num_hash_ones[new_num./(100000)] + " hundred")
+      new_num = new_num.%(100000)
+    end
+    while new_num >= 10000 do
+      word.push(num_hash_tens[new_num./(10000)])
+      new_num = new_num.%(10000)
+    end
+    while new_num >= 1000 do
+      word.push(num_hash_ones[new_num./(1000)] + " thousand")
       new_num = new_num.%(1000)
     end
-    while new_num > 100 do
-      word = word + num_hash_ones[new_num./(100)] + " hundred "
+    while new_num >= 100 do
+      word.push(num_hash_ones[new_num./(100)] + " hundred")
       new_num = new_num.%(100)
     end
     while new_num > 19 do
-      word = word + num_hash_tens[new_num./(10)] + " "
+      word.push(num_hash_tens[new_num./(10)])
       new_num = new_num.%(10)
     end
-    while new_num > 10 do
-      word = word + num_hash_tens[new_num]
+    while new_num >= 10 do
+      word.push(num_hash_tens[new_num])
       new_num = 0
     end
     while new_num > 0 do
-      word = word + num_hash_ones[new_num]
+      word.push(num_hash_ones[new_num])
       new_num = 0
     end
 
@@ -39,6 +47,6 @@ class Fixnum
     # elsif self < 1000000
     #
     # end
-    word
+    word.join(' ')
   end
 end
--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -17,4 +17,13 @@ describe('Fixnum#num_to_word') do
   it("returns a word for a number less than ten thousand") do
     expect(3746.num_to_word()).to(eq('three thousand seven hundred forty six'))
   end
+  it("returns a word for a number less than one hundred thousand") do
+    expect(83746.num_to_word()).to(eq('eighty three thousand seven hundred forty six'))
+  end
+  it("returns a word for a thousand") do
+    expect(1000.num_to_word()).to(eq('one thousand'))
+  end
+  it("returns a word for a number less than one million") do
+    expect(283746.num_to_word()).to(eq('two hundred eighty three thousand seven hundred forty six'))
+  end
 end
