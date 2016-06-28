3bcdafbfd1b3b2c8f2af2f8178aa4a82990d2767
Caleb Stokka and Tony Fabery <caleb+tony@>
2016-06-28 14:10:39 -0700

    returns a word for a number less than one quintillion

--- spec/number_to_word_spec.rb
+++ spec/number_to_word_spec.rb
@@ -5,6 +5,9 @@ describe('Fixnum#num_to_word') do
   it("returns a word for a single digit") do
     expect(9.num_to_word()).to(eq('nine'))
   end
+  it("returns a word for a number up to ten") do
+    expect(10.num_to_word()).to(eq('ten'))
+  end
   it("returns a word for a number up to twenty") do
     expect(18.num_to_word()).to(eq('eighteen'))
   end
@@ -14,16 +17,34 @@ describe('Fixnum#num_to_word') do
   it("returns a word for a number less than one thousand") do
     expect(678.num_to_word()).to(eq('six hundred seventy eight'))
   end
+  it("returns a word for a thousand") do
+    expect(1000.num_to_word()).to(eq('one thousand'))
+  end
   it("returns a word for a number less than ten thousand") do
     expect(3746.num_to_word()).to(eq('three thousand seven hundred forty six'))
+  end
+  it("returns a word for ten thousand") do
+    expect(10000.num_to_word()).to(eq('ten thousand'))
   end
   it("returns a word for a number less than one hundred thousand") do
     expect(83746.num_to_word()).to(eq('eighty three thousand seven hundred forty six'))
   end
-  it("returns a word for a thousand") do
-    expect(1000.num_to_word()).to(eq('one thousand'))
+  it("returns a word for a hundred thousand") do
+    expect(100000.num_to_word()).to(eq('one hundred thousand'))
   end
   it("returns a word for a number less than one million") do
     expect(283746.num_to_word()).to(eq('two hundred eighty three thousand seven hundred forty six'))
+  end
+  it("returns a word for a number less than one billion") do
+    expect(4283746.num_to_word()).to(eq('four million two hundred eighty three thousand seven hundred forty six'))
+  end
+  it("returns a word for a number less than one trillion") do
+    expect(1245283746.num_to_word()).to(eq('one billion two hundred forty five million two hundred eighty three thousand seven hundred forty six'))
+  end
+  it("returns a word for a number less than one quadrillion") do
+    expect(5612245283746.num_to_word()).to(eq('five trillion six hundred twelve billion two hundred forty five million two hundred eighty three thousand seven hundred forty six'))
+  end
+  it("returns a word for a number less than one quintillion") do
+    expect(1005612245283746.num_to_word()).to(eq('one quadrillion five trillion six hundred twelve billion two hundred forty five million two hundred eighty three thousand seven hundred forty six'))
   end
 end
